b2b87d970e02c3d3b2c596722ee5588111b4ec33
arseniy54 <91014197+arseniy54@users.noreply.github.com>
2022-06-04 20:57:10 +0300

    s

---
+++ OpenCart/upload/links/language/ru-ru/common/header.php
@@ -0,0 +1,10 @@
+<?php
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Text
+$_['lk']                 = 'Личный кабинет';
+$_['sum']                = 'Сумма:';
+$_['tovars']             = 'Товаров:';
+$_['phone']              = '+7 (999) 999-99-99';
+$_['town']               = 'Выберите ваш город:';
---
+++ OpenCart/upload/register/controller/common/search.php
@@ -0,0 +1,28 @@
+<?php
+class ControllerCommonSearch extends Controller {
+	public function index() {
+		$this->load->language('common/search');
+
+		$data['text_search'] = $this->language->get('text_search');
+
+		if (isset($this->request->get['search'])) {
+			$data['search'] = $this->request->get['search'];
+		} else {
+			$data['search'] = '';
+		}
+
+		return $this->load->view('common/search', $data);
+
+
+        if (is_file(DIR_IMAGE . $this->config->get('config_icon'))) {
+            $this->document->addLink($server . 'image/' . $this->config->get('config_icon'), 'icon');
+        }
+
+        if (is_file(DIR_IMAGE . $this->config->get('config_loop'))) {
+            $data['loop'] = $server . 'image/header' . $this->config->get('config_loop');
+        } else {
+            $data['loop'] = '';
+        }
+	}
+
+}
